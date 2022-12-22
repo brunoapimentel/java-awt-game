69e70b161adbc3f9206d76bd468d03d63df8e4fd
Bruno Pimentel <brunoapimentelgmail.com>
2022-12-22 00:46:14 -0300

    Adiciona handler para key press na engine
    Também remove a responsabilidade da gravidade dessa classe.

--- src/engine/GameEngine.java
+++ src/engine/GameEngine.java
@@ -1,21 +1,31 @@
 package engine;
 
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.util.ArrayList;
 import java.util.List;
 
-public class GameEngine extends Frame{
-    private int ticks = 5;
+public class GameEngine extends Frame {
+    private int ticks = 30;
     private List<GameObject> objects = new ArrayList<>();
-    private int gravity = -10;
-
     private Toolkit t = Toolkit.getDefaultToolkit();
+
+    Image backgroundImage;
+    int width;
+    int height;
+
+    public GameEngine(int width, int height, Image backgroundImage) {
+        this.width = width;
+        this.height = height;
+        this.backgroundImage = backgroundImage;
+    }
 
     public void start() {
         setVisible(true);
-        setSize(600, 600);
+        setSize(width, height);
         addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
@@ -25,19 +35,28 @@ public class GameEngine extends Frame{
 
         new Thread(() -> {
             while (true) {
-                sleep(1000 / 30);
-
+                sleep(1000 / ticks);
                 t.sync();
                 repaint();
-
             }
         }).start();
+
+        addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                for (GameObject object : objects) {
+                    object.onKeyPress(e);
+                }
+            }
+        });
     }
 
     @Override
     public void paint(Graphics g) {
+        g.drawImage(backgroundImage, 0, 0, null);
+
         for (GameObject object : objects) {
-            object.paint(ticks, gravity, g);
+            object.paint(g);
         }
     }
 
--- src/engine/GameObject.java
+++ src/engine/GameObject.java
@@ -1,21 +1,14 @@
 package engine;
 
-import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
 
-public class GameObject {
-
-    // per second
-    private int accelerationX = 0;
-    private int accelerationY = 0;
-
-    private int x = 0;
-    private int y = 0;
-
+public abstract class GameObject {
     private Image image;
+    protected int x;
+    protected int y;
 
-    public GameObject(Image image) {
-        this.image = image;
+    public void onKeyPress(KeyEvent e) {
     }
 
     public GameObject(int x, int y, Image image) {
@@ -24,30 +17,7 @@ public class GameObject {
         this.image = image;
     }
 
-    public Image getImage() {
-        return image;
-    }
-
-    public void setAccelerationX(int accelerationX) {
-        this.accelerationX = accelerationX;
-    }
-
-    public void setAccelerationY(int accelerationY) {
-        this.accelerationY = accelerationY;
-    }
-
-    public void paint(int ticks, int gravity, Graphics g) {
-        int nextX = x + (accelerationX/ticks);
-        int nextY = y + (accelerationY/ticks);
-
-        if (nextY > 400) {
-            nextY = 400;
-        }
-
-        g.drawImage(image, nextX, nextY, null);
-
-        accelerationY -= (gravity / ticks);
-        x = nextX;
-        y = nextY;
+    public void paint(Graphics g) {
+        g.drawImage(image, x, y, null);
     }
 }
